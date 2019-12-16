69cc02a1d16957dcccb138c9b654cb9b65cf3ba2
Jacob Mischka <jacob@mischka.me>
2019-12-15 18:26:38 -0600

    Idk, it compiles I think?

--- src/Base.thy
+++ src/Base.thy
@@ -4,43 +4,52 @@ imports Main
 
 begin
 
-fun add :: "nat => nat => nat" where
-"add 0 n = n" |
-"add (Suc m) n = Suc (add m n)"
+definition
+  shift :: "(nat \<Rightarrow> 'a) \<Rightarrow> nat \<Rightarrow> 'a \<Rightarrow> nat \<Rightarrow> 'a"  ("_, _:_" [90, 0, 0] 91) where
+  "\<Gamma>, i:a = (\<lambda>j. if j < i then \<Gamma> j else if j = i then a else \<Gamma> (j - 1))"
 
-lemma add_02: "add n 0 = n"
-	apply (induction n)
-	apply (auto)
-	done
+text \<open>
+lemma shift_eq [simp]: "i = j \<Longrightarrow> (\<Gamma>, i:T) j = T"
+  by (simp add: shift_def)
 
-end
+lemma shift_gt [simp]: "j < i \<Longrightarrow> (\<Gamma>, i:T) j = \<Gamma> j"
+  by (simp add: shift_def)
+
+lemma shift_lt [simp]: "i < j \<Longrightarrow> (\<Gamma>, i:T) j = \<Gamma> (j - 1)"
+  by (simp add: shift_def)
+
+lemma shift_commute [simp]: "\<Gamma>, i:U, 0:T = \<Gamma>, 0:T, Suc i:U"
+  by (rule ext) (simp_all add: shift_def split: nat.split)
+\<close>
 
 type_synonym varName = nat
 type_synonym typeName = nat
 
-type_synonym varCtx = "varName \<rightharpoonup> typeName"
+datatype "type" =
+	Atom typeName
+	| Fun type type (infixr "->" 200)
 
-consts fn
+type_synonym varCtx = "varName => type"
+
 
 datatype "term" =
-	Abstraction x::varName "typeName" t::"term"
-	| Var "varName"
-	| Application "exp exp"
+	Var "varName"
+	| Abs varName "type" "term"
+	| App "term" "term" (infixl "\<degree>" 200)
 
 inductive "value" :: "term => bool" where
 	"value (Abstraction _ _ _)"
 
-consts
-	typing :: (varCtx * exp * typeName) set
-
 inductive
-	typing :: [varCtx, exp, typeName] ==> bool ("_ ⊢ _ : _" [80, 80, 80] 80)
+	typing :: "varCtx => term => type => bool" ("_ ⊢ _ : _" [50, 50, 50] 50)
 
 	where
 
-	t_var : "Γ(x) = Some T ==> Γ ⊢ (Var x) : T"
-	| t_abs : "\<lbrakk> Γ ⊢ "
+	t_abs : "\<Gamma>, x:T1 ⊢ t2 : T2 ==> \<Gamma> ⊢ Abs x T1 t : (T1 -> T2)"
+	| t_var : "\<Gamma>(x) = T ==> \<Gamma> ⊢ (Var x) : T"
+	| t_app : "\<Gamma> ⊢ t1 : (T1 -> T2) ==> \<Gamma> ⊢ t2 : T1 ==> \<Gamma>  ⊢ (t1 \<degree> t2) : T2"
 
-translations
-	Γ ⊢ t : T 
-
+inductive_cases typing_elims [elim!]:
+  "\<Gamma> ⊢ Var x : T"
+  "\<Gamma> ⊢ t \<degree> u : T"
+  "\<Gamma> ⊢ Abs x T1 t : T"
